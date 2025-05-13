832dbab8bc90ab1156914c4df8ac4c4c6d04f491
zZveruga <tz35478@gmail.com>
2025-05-13 23:07:37 +0300

    lesson_5

---
+++ lesson_05/lesson05_task4.py
@@ -0,0 +1,29 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get("https://the-internet.herokuapp.com/login")
+
+input_user = driver.find_element(By.CSS_SELECTOR, "#username")
+input_user.send_keys("tomsmith")
+sleep(5)
+
+input_pass = driver.find_element(By.CSS_SELECTOR, "#password")
+input_pass.send_keys(" SuperSecretPassword!")
+sleep(5)
+
+login_button = driver.find_element(By.CSS_SELECTOR, "button.radius")
+login_button.click()
+sleep(5)
+
+success_message = driver.find_element(By.CSS_SELECTOR, ".flash.success")
+print(success_message.text)
+
+driver.quit()
